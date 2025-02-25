3efa396c75d37fc7556a5be6002d9f7c4030741c
charankumar-g <charankumar.career@gmail.com>
2025-02-25 20:13:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("Welcome to my portfolio site!");
+document.querySelector(".arrow").addEventListener("click", function () {
+    window.scrollTo({
+        top: window.innerHeight,
+        behavior: "smooth"
+    });
+});
