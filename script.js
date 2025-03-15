9a4814a239dfbbcc74a52d79d2ee00ad9f6b0c69
CHARAN KUMAR G <charankumar.career@gmail.com>
2025-03-15 10:44:38 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-console.log("Welcome to my portfolio site!");
-document.querySelector(".arrow").addEventListener("click", function () {
-    window.scrollTo({
-        top: window.innerHeight,
-        behavior: "smooth"
-    });
-});
